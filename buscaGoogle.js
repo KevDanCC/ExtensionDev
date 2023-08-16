9a9c517264fa0f368b58df3b459965c978e2da4b
Kevin Cisneros <kev.cisn@hotmail.com>
2023-08-15 19:05:10 -0600

    First steps on extension developer

---
+++ buscaGoogle.js
@@ -0,0 +1,26 @@
+//buscaGoogle.js
+//Buscamos el input text llamado q y le escribimos programador novato
+let input=document.querySelector('[name="q"]');
+if(input)
+input.value="programador novato";
+//Creamos el evento click
+var evt = new MouseEvent('click', {
+    bubbles: true,
+	cancelable: true,
+	view: window
+});
+let note = document.querySelector('.FPdoLc');
+
+note.appendChild(append);
+
+
+
+//Si no se cancela el evento lo ejecutamos
+// var canceled = !document.querySelector('input[name="btnK"]').dispatchEvent(evt);
+let append= document.createElement('div');
+append.innerHTML=`
+<div>
+Esto es texto extra:
+<a href="https://www.facebook.com">Clickme!</a>
+</div>
+`
